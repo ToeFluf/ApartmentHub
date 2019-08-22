7c6e4875b02262eb74f2c91efc5a730fa56a7ee5
ToeFluf <jakday777@gmail.com>
2019-08-21 23:18:58 -0500

    Initial server start

---
+++ server.js
@@ -0,0 +1,25 @@
+/** 
+ * @title server.js
+ * @authors Reece Mathews and Daniel Schmidt
+ * @description Basic Server for Rasp PI for funs stuff
+ * */
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const process = require('process')
+
+const PORT = 3000;
+const HOST = '0.0.0.0'
+
+const reqHandler = (req, res)=>{
+	console.log(req.url);
+	res.end("Connected");
+};
+
+const server = http.createServer(reqHandler);
+
+server.listen(PORT, HOST, function(){
+	console.log("Listening at " + HOST + " on port " + 3000);
+}).on('connect',(req, socket, head)=>{
+	console.log("Request: " + req +'\n' + socket);
+});
